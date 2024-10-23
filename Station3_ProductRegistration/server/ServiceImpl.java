9e19b5e825ec4be7ddf48282f85e3dedc5214efe
SnehaKoirala <345788@via.dk>
2024-10-23 13:34:05 +0200

    station 3 classes
    station 3 classes

---
+++ Station3_ProductRegistration/server/ServiceImpl.java
@@ -0,0 +1,12 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class ServiceImpl extends UnicastRemoteObject implements Service {
+
+    protected ServiceImpl() throws RemoteException {
+
+    }
+
+
+}
